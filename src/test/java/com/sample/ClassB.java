4665862813f31a57065cbb259d0507717d4495a0
Divyaslm <dharshinislm721@gmail.com>
2022-11-07 12:18:04 +0530

    First time push

---
+++ src/test/java/com/sample/ClassB.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class ClassB {
+
+	public void GreensOMR() {
+        System.out.println("OMR");
+}
+	public void GreensAdayar() {
+		System.out.println("Adayar");
+	}
+	public void GreensProur() {
+		System.out.println("Prour");
+	}
+}
